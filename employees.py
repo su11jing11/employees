9d98e115045125a6d6b70cf4cdd8f51921cf9e35
su11jing11 <jsu2@student.unimelb.edu.au>
2019-03-21 23:26:30 +0800

    Add files via upload

---
+++ employees.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+txtfile = 'employees_dataset.csv'
+
+
+
+
+class Candidate:
+    def __init__(self, degree, education, skills, experience, position):
+        self.degree = degree
+        self.education = education
+        self.skills = skills
+        self.experience = experience
+        self.position = position
+
+    def printInfo(self):
+        print("This candidate has degree of ", self.degree, ", from ", self.education, "and has skills ", self.skills,
+              "\n has been worked for ", self.experience, " as ", self.position)
+
+
+def readFromFile(txtfile):
+    f = open(txtfile, 'r')
+    lines = f.readlines()
+    candidates = []
+    for i in range(len(lines)):
+        if i == 0:
+            continue
+        text = lines[i].split(',')
+        degree = text[0]
+        education = text[1]
+        skills = text[2].split(';')
+        experience = text[3].split(';')
+        position = text[4].split('\n')
+        candidates.append(Candidate(degree, education, skills, experience, position[0]))
+    return candidates
+    # candidates[0].printInfo()
+
+if __name__ == "__main__":
+    candidates=readFromFile(txtfile)
+    skillsets=[]
+    companysets=[]
+    positionsets=[]
+    degreesets=[]
+    skillsets.append([candidates[0].skills[0],0,0,0])
+    companysets.append([candidates[0].experience[0],0,0,0])
+    positionsets.append(candidates[0].position)
+    degreesets.append([candidates[0].degree,0,0,0])
+    labelset=[]
+    for i in range(len(candidates)):
+        for j in range(len(candidates[i].skills)):
+            for k in range(len(skillsets)):
+                if candidates[i].skills[j] == skillsets[k][0]:
+                    break
+                if k==len(skillsets)-1:
+                    skillsets.append([candidates[i].skills[j],0,0,0])
+        for j in range(len(candidates[i].experience)):
+            for k in range(len(companysets)):
+                if candidates[i].experience[j] == companysets[k][0]:
+                    break
+                if k==len(companysets)-1:
+                    companysets.append([candidates[i].experience[j],0,0,0])
+        for j in range(len(positionsets)):
+            if candidates[i].position == positionsets[j]:
+                break
+            if j==len(positionsets)-1:
+                positionsets.append(candidates[i].position)
+        for j in range(len(degreesets)):
+            if candidates[i].degree == degreesets[j][0]:
+                break
+            if j==len(degreesets)-1:
+                degreesets.append([candidates[i].degree,0,0,0])
+        labelset.append(candidates[i].position)
+    # print(len(skillsets))
+    # print(positionsets)
+    # print(degreesets)
+    X_train, X_test, y_train, y_test = train_test_split(candidates, labelset, random_state=int(time.time()))
+    # X_train[0].printInfo()
+    for i in range(len(X_train)):
+        for j in range(len(positionsets)):
+            if X_train[i].position == positionsets[j]:
+                for k in range(len(X_train[i].skills)):
+                    for index in range(len(skillsets)):
+                        if X_train[i].skills[k] == skillsets[index][0]:
+                            skillsets[index][j+1] += 1
+                for index in range(len(degreesets)):
+                    if X_train[i].degree == degreesets[index][0]:
+                        degreesets[index][j+1]+=1
+    id=int(time.time())%len(X_train)
+    X_train[id].printInfo()
+    degree_pre=0;
+    targetpersion=X_train[id]
+    for i in range(len(degreesets)):
+        if degreesets[i][0]==targetpersion.degree:
+            degree_pre=i
+            break
+    print(degree_pre)
+    dev_pre=0
+    manager_pre=0
+    qa_pre=0
+    for i in range(len(targetpersion.skills)):
+        for j in range(len(skillsets)):
+            if(targetpersion.skills[i]==skillsets[j][0]):
+                max=skillsets[j][1]
+                maxpos=1
+                if skillsets[j][2]>max:
+                    max=skillsets[j][2]
+                    maxpos=2
+                if skillsets[j][3]>max:
+                    max=skillsets[j][3]
+                    maxpos=3
+                if maxpos==1:
+                    dev_pre+=1
+                if maxpos==2:
+                    manager_pre+=1
+                if maxpos==3:
+                    qa_pre+=1
+    x=[dev_pre,manager_pre,qa_pre]
+    predict=np.where(x==np.max(x))
+    print(predict)
+    print(y_train[id])
+    # print(positionsets)
+    # print(degreesets)
+    print(skillsets)
